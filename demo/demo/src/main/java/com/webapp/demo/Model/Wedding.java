6da9be1c8832c6c257d4aeceaf486c671ed4c91d
nayana-sisil <nayanasisil@gmail.com>
2025-05-03 21:04:15 +0530

    entity classes added

---
+++ demo/demo/src/main/java/com/webapp/demo/Model/Wedding.java
@@ -0,0 +1,17 @@
+package com.webapp.demo.Model;
+
+import java.time.LocalDate;
+
+public class Wedding {
+
+    protected int clientId;
+
+    protected String groomName;
+
+    protected String brideName;
+
+    protected LocalDate weddingDate;
+
+    protected int guestCount;
+    
+}
