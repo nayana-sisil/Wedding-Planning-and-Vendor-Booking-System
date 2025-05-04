ab8a7701d6dcb33a007f43222438f38dafdd1770
IT24101303 <IT24101303@my.sliit.lk>
2025-05-04 14:22:31 +0530

    Added Spring Boot project for vendor management

---
+++ src/main/java/com/vendor/vendormanagement/VendormanagementApplication.java
@@ -0,0 +1,13 @@
+package com.vendor.vendormanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendormanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendormanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vendor/vendormanagement/VendormanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendor.vendormanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendormanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
