f3fb4392da4a031d0ca9a9d747573ae061047284
IT24101303 <IT24101303@my.sliit.lk>
2025-05-04 15:30:26 +0530

    added vendor service but methods not working yet

--- vendormanagement/src/main/java/com/vendor/vendormanagement/service/VendorService.java
+++ vendormanagement/src/main/java/com/vendor/vendormanagement/service/VendorService.java
@@ -1,4 +1,27 @@
 package com.vendor.vendormanagement.service;
 
+//
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.vendor.vendormanagement.model.Vendor;
+
+//
+
+@Service
 public class VendorService {
+
+    public List<Vendor> gettAllVendors() {
+
+        //
+
+        return null;
+    }
+
+    public Vendor addVendor(Vendor vendors) {
+
+        //
+
+        return vendors;
+    }
 }
