3cc14a391f1763ad3e72bd4c5cfd3d1da7d74167
IT24101303 <IT24101303@my.sliit.lk>
2025-05-05 14:59:32 +0530

    updated VendormanagementApplication.java, VendorService, Controller + added static frontend to test add, search, list, update, delete vendors

--- vendormanagement/src/main/java/com/vendor/vendormanagement/VendormanagementApplication.java
+++ vendormanagement/src/main/java/com/vendor/vendormanagement/VendormanagementApplication.java
@@ -2,8 +2,9 @@ package com.vendor.vendormanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class VendormanagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendormanagementApplication.class, args);
